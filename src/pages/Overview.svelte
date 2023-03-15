4ecc3eff33a6ff01598bbb0d265483534db386de
plsTrustMeImAnEngineer <04samu04@gmail.com>
2023-03-15 23:31:28 +0100

    Changed client:only

--- src/pages/Overview.svelte
+++ src/pages/Overview.svelte
@@ -33,8 +33,8 @@
 
 <style>
     #page {
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         display: flex;
         flex-direction: column;
         justify-content: flex-start;

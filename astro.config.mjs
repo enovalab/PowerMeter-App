989bc2b84d8eba4e5f349b118ad3e6c414530a62
Samuel_Lehnen <samuel.lehnen@katek-group.com>
2023-03-16 08:22:04 +0100

    Test deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import svelte from '@astrojs/svelte';
 // https://astro.build/config
 export default defineConfig({
     site: "https://enovalab.github.io",
-    base: "/PowerMeter-App",
     integrations: [svelte()]
 });
